886ec9e94b03ec6c674c9cc0510f1f5e41134127
alwinihza <alwin.ihza.f@mail.ugm.ac.id>
2018-11-17 18:08:12 +0700

    fix database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
         'secret': 'ENTER_YOUR_JWT_SECRET_HERE',
-        'database': 'mongodb://accountUser:password@127.0.0.1:27017/products?ssl=false',
-        'password': '$2a$10$/YLEhYFB2IXk4HACW2Hrd.F13FPipvKw.5f8cmCeGjvS8zEz/b19C'//ex: $2a$10$nT27sEgYaAjUH6DxWp1NAeXSfI.OEz4KIz73NMuC68b8d5erkTIYO
+        'database': 'mongodb://kpu:ft2019@ds041167.mlab.com:41167/kpuft',
+        'password': '$2a$10$gN7hhyGNWnJQrTttxH.njOiusICCVPppQ3G/frRzaYV7h2Xy/lXTy'//ex: $2a$10$nT27sEgYaAjUH6DxWp1NAeXSfI.OEz4KIz73NMuC68b8d5erkTIYO
 };
